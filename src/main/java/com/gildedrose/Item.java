c2e6878fd8442c66565a3080b611a86328f7d724
soufiane chaieb <s.chaieb19@gmail.com>
2022-12-21 17:49:21 +0100

    last commit

--- src/main/java/com/gildedrose/Item.java
+++ src/main/java/com/gildedrose/Item.java
@@ -1,7 +1,11 @@
 package com.gildedrose;
 
 public class Item {
-
+    public final static  String AGED_BRIE="Aged Brie";
+    public final static  String  SULFURAS="Sulfuras, Hand of Ragnaros";
+    public final static  String  BACKSTAGE="Backstage passes to a TAFKAL80ETC concert";
+    public final static  String CAKE="Conjured Mana Cake";
+  
     public String name;
 
     public int sellIn;
@@ -18,4 +22,67 @@ public class Item {
    public String toString() {
         return this.name + ", " + this.sellIn + ", " + this.quality;
     }
+    
+    /**
+     * 
+     */
+    public void updateQuality() {
+        if(this.name.equals(SULFURAS)) {
+            return;
+        }
+
+            this.sellIn--;
+
+        switch(this.name){
+            case AGED_BRIE:
+                  this.quality++;
+                  if( this.sellIn<0){
+                    this.quality++;
+                      }
+            break;
+            case BACKSTAGE:
+              
+                    this.quality++;
+                    if( this.sellIn<10){
+                        this.quality++;
+                    }
+                    if( this.sellIn<5){
+                        this.quality++;
+                    }
+                    if( this.sellIn<0){
+                        this.quality=0;
+                    } 
+                
+            break;
+            case CAKE:
+                  if(this.sellIn<0){
+                    this.quality-=4;
+                  }else{
+                    this.quality-=2;
+                  }
+            break;
+                
+            default :
+                    if(this.sellIn<0){
+                        this.quality-=2;
+                    }else{
+                        this.quality-=1;
+                            }
+            break; 
+                    
+           
+            }
+            testQuality();
+               
+    }
+    private void testQuality(){
+        if(this.quality>50){
+            this.quality=50;
+                    }  
+        if(this.quality<0){
+            this.quality=0;
+    }
+
+    }
 }
+
--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -12,7 +12,70 @@ class GildedRoseTest {
     Item element = new Item("foo", 0, 0);
     GildedRose app = new GildedRose(new Item[] {element});
     app.updateQuality();
-    assertThat(element.name, is("FIXME"));
+    assertThat(element.name, is("foo"));
   }
+
+  @Test
+  @DisplayName("Test that the quality will decrement")
+  void testdecrementation() {
+    Item[] items = new Item[] {new Item("boo", 2, 0),
+    new Item("foo", 2, 6),
+    new Item(Item.CAKE, -1, 49),
+    new Item(Item.CAKE, 0, 49),
+    new Item(Item.CAKE, 1, 49),
+    new Item("boo", -1, 0)
+  };
+    GildedRose app = new GildedRose(items);
+    app.updateQuality();
+  
+    assertThat(app.items[0].quality, is(0));
+    assertThat(app.items[1].quality, is(5));
+    assertThat(app.items[2].quality, is(45));
+    assertThat(app.items[3].quality, is(45));
+    assertThat(app.items[4].quality, is(47));
+    assertThat(app.items[5].quality, is(0));
+
+}
+
+
+@Test
+@DisplayName("Test  quality increases as time goes by")
+void quality_increases_in_time() {
+  Item[] items = new Item[] {new Item(Item.AGED_BRIE, 2, 0),
+    new Item(Item.AGED_BRIE, 2, 50),
+    new Item(Item.BACKSTAGE, 4, 44),
+    new Item(Item.SULFURAS, 0, 50),
+    new Item(Item.AGED_BRIE, 0, 2),
+    new Item(Item.BACKSTAGE, 11, 17),
+    new Item(Item.BACKSTAGE, 0, 49),
+    new Item(Item.SULFURAS, 0, 40),
+    new Item(Item.AGED_BRIE, 1, 40),
+    new Item(Item.SULFURAS, -1, 49),
+      
+    };
+    GildedRose app = new GildedRose(items);
+    app.updateQuality();
+    assertThat(app.items[0].quality, is(1));
+    assertThat(app.items[1].quality, is(50));
+    assertThat(app.items[2].quality, is(47));
+    assertThat(app.items[3].quality, is(50));
+    assertThat(app.items[4].quality, is(4));
+    assertThat(app.items[4].sellIn, is(-1));
+    assertThat(app.items[5].quality, is(18));
+    assertThat(app.items[6].quality, is(0));
+    assertThat(app.items[7].quality, is(40));
+    assertThat(app.items[8].quality, is(41));
+    assertThat(app.items[9].quality, is(49));
+    
+}
+
+@Test
+  @DisplayName("Test tostring")
+  void testtostring() 
+  {
+      Item element =  new Item ("Aged Brie",0,40  );
+      assertThat(element.toString(), is("Aged Brie, 0, 40"));
+   
+    }
 
 }
